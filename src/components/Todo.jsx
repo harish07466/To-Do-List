e27a80751241e93cd4d1c5399b5bdbcb7c93d711
harish07466 <harishmkumbar@gmail.com>
2025-11-13 21:05:56 +0530

    first commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Form from "./Form";
 import TodoList from "./TodoList";
 import Footer from "./Footer";
 
 export default function Todo() {
-  const [todos, setTodos] = useState([]);
+  // Load from localStorage on first render
+  const [todos, setTodos] = useState(() => {
+    const saved = localStorage.getItem("todos");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  // Save automatically whenever todos change
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   const CompletedTodos = todos.filter((todo) => todo.done).length;
   const TotalTodos = todos.length;
+
   return (
     <div>
       <Form todos={todos} setTodos={setTodos} />
